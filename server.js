c770e828148d00ebf06112d7435c4d18b9f45238
Ankur Rohilla <ankurrohilla9691914@gmail.com>
2021-05-25 16:18:25 +0530

    initial routes

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-if(process.env.NODE_ENV !== 'production')
-{
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-const express = require('express');
-const app=express();
-const expressLayouts =require('express-ejs-layouts');
-const path = require('path');
-const mongoose = require('mongoose');
-
-const indexRouter = require('./routes/index')
-
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'/views'))
-app.set('layout','layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
-
-//! database url have to be changed by mongo atlas
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true,
-})
-const db = mongoose.connection;
-db.on('error',error=>console.error(error));
-db.once('open',()=>console.log('connected to mongoose'));
-
-
-
-
-app.use('/',indexRouter)
-
-app.listen(process.env.PORT ||3000)
+  }
+  
+  const express = require('express')
+  const app = express()
+  const expressLayouts = require('express-ejs-layouts')
+  const bodyParser = require('body-parser')
+  
+  const indexRouter = require('./routes/index')
+  const authorRouter = require('./routes/authors')
+  
+  app.set('view engine', 'ejs')
+  app.set('views', __dirname + '/views')
+  app.set('layout', 'layouts/layout')
+  app.use(expressLayouts)
+  app.use(express.static('public'))
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+  
+  const mongoose = require('mongoose')
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
+  
+  app.use('/', indexRouter)
+  app.use('/authors', authorRouter)
+  
+  app.listen(process.env.PORT || 3000)
